6b7e70ac0f4905bb1168bc52df9f67aaef472e43
Hiroki Takeda <takedahiroki@gmail.com>
2021-01-01 09:42:58 +0900

    オープンデータCSVを辞書のリストに変換するよう変更。

--- hinanbasho/scraper.py
+++ hinanbasho/scraper.py
@@ -4,11 +4,11 @@ import pandas as pd
 import requests
 
 
-class DownloadedCSV:
+class Scraper:
     """旭川市オープンデータライブラリからCSVをダウンロードしてテキスト要素の二次元配列に格納する
 
     Attributes:
-        lists(list of lists): CSVを二次元配列に変換したデータ
+        lists(list of dicts): CSVの各行を辞書にしてリストに格納したデータ
 
     """
 
@@ -19,25 +19,29 @@ class DownloadedCSV:
 
         """
         self.__lists = list()
-
         response = requests.get(url)
         csv_content = io.BytesIO(response.content)
-
-        df = pd.read_csv(csv_content, encoding="cp932", header=None, dtype=str)
+        df = pd.read_csv(csv_content, encoding="cp932", header=0, dtype=str)
         df.replace(np.nan, "", inplace=True)
-
-        self.__lists = df.values.tolist()
+        for row in df.values.tolist():
+            # オープンデータの豊西会館だけ緯度経度が抜けているので対策する
+            if row[0] == "豊西会館":
+                latitude = 43.6832208
+                longitude = 142.1762534
+            else:
+                latitude = float(row[5])
+                longitude = float(row[6])
+            self.__lists.append(
+                {
+                    "site_name": row[0],
+                    "postal_code": row[1],
+                    "address": row[2],
+                    "phone_number": row[3],
+                    "latitude": latitude,
+                    "longitude": longitude,
+                }
+            )
 
     @property
     def lists(self) -> list:
         return self.__lists
-
-
-if __name__ == "__main__":
-    url = (
-        "https://www.city.asahikawa.hokkaido.jp/kurashi/320/321/d053843_d/fil/"
-        + "012041_hinanbasho_list.csv"
-    )
-    downloaded_csv = DownloadedCSV(url)
-    for row in downloaded_csv.lists:
-        print(row)
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,5 +1,5 @@
 import unittest
-from hinanbasho.scraper import DownloadedCSV
+from hinanbasho.scraper import Scraper
 from requests import ConnectionError
 from requests import HTTPError
 from requests import RequestException
@@ -7,28 +7,51 @@ from requests import Timeout
 from unittest.mock import Mock, patch
 
 
-class TextDownloadedCSV(unittest.TestCase):
+class TestScraper(unittest.TestCase):
     @patch("hinanbasho.scraper.requests")
     def test_lists(self, mock_requests):
-        csv_content = "a,b,c" + "\r\n" + "1,2,3" + "\r\n"
-        mock_requests.get.return_value = Mock(
-            status_code=200, content=csv_content.encode()
+        csv_content = (
+            "施設名,郵便番号,住所,電話番号,ファクス番号,地図の緯度,地図の経度"
+            + "\r\n"
+            + "常磐公園,070-0044,北海道旭川市常磐公園,0166-23-8961,なし,43.7748548,142.3578223"
+            + "\r\n"
+            + "豊西会館,074-1182,北海道旭川市神居町豊里,なし,なし,,"
+            + "\r\n"
         )
-        expect = [["a", "b", "c"], ["1", "2", "3"]]
-        downloaded_csv = DownloadedCSV("http://dummy.local")
+        mock_requests.get.return_value = Mock(
+            status_code=200, content=csv_content.encode("cp932")
+        )
+        expect = [
+            {
+                "site_name": "常磐公園",
+                "postal_code": "070-0044",
+                "address": "北海道旭川市常磐公園",
+                "phone_number": "0166-23-8961",
+                "latitude": 43.7748548,
+                "longitude": 142.3578223,
+            }, {
+                "site_name": "豊西会館",
+                "postal_code": "074-1182",
+                "address": "北海道旭川市神居町豊里",
+                "phone_number": "なし",
+                "latitude": 43.6832208,
+                "longitude": 142.1762534,
+            },
+        ]
+        downloaded_csv = Scraper("http://dummy.local")
         self.assertEqual(downloaded_csv.lists, expect)
 
         mock_requests.get.side_effect = Timeout("Dummy Error.")
         with self.assertRaises(RequestException):
-            DownloadedCSV("http://dummy.local")
+            Scraper("http://dummy.local")
 
         mock_requests.get.side_effect = HTTPError("Dummy Error.")
         with self.assertRaises(RequestException):
-            DownloadedCSV("http://dummy.local")
+            Scraper("http://dummy.local")
 
         mock_requests.get.side_effect = ConnectionError("Dummy Error.")
         with self.assertRaises(RequestException):
-            DownloadedCSV("http://dummy.local")
+            Scraper("http://dummy.local")
 
 
 if __name__ == "__main__":
