06d45c84183a56f4a13eb5d70e4d3609daf12052
Hiroki Takeda <takedahiroki@gmail.com>
2021-03-14 12:27:56 +0900

    fixed.

--- hinanbasho/config.py
+++ hinanbasho/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    DATABASE_URL = os.environ.get("HINANBASHO_DB_URL")
+    DATABASE_URL = os.environ.get("DATABASE_URL")
     OPENDATA_URL = (
         "https://www.city.asahikawa.hokkaido.jp/kurashi/320/321/d053843_d/fil/"
         + "012041_hinanbasho_list.csv"
